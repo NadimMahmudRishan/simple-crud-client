c6fdf5b5a10f121c4f50561fc10ecd7ef996cf8d
NadimMahmudRishan <resunroy420@gmail.com>
2025-01-28 01:01:16 +0600

    get data from the form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
+  const handelAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
 
   return (
     <>
+      <form onSubmit={handelAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" />
+      </form>
+
       <h1>Simple CRUD operation</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
